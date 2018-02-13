7f8c4d7bc262f86d6b55178f938f450ad32811e4
localedtech <bridget@localed.co>
2018-02-13 13:06:56 -0500

    update views.py resume fun def doc string and variable names

--- project1/week3/project1/resume/views.py
+++ project1/week3/project1/resume/views.py
@@ -5,11 +5,11 @@ from .models import Education, Experience
 
 def resume(request):
 	"""
-	This function returns education and experience objects in the resume/ URL.
+	This function returns education and experience objects in the URL resume/.
 	request: URL request resume/ from user
 	return: the URL resume/ with all education and experience objects
 	"""
-	qs = Education.objects.all()
-	qs2 = Experience.objects.all()
-	context = {'Education' : qs, 'Experience': qs2}
+	query_set = Education.objects.all()
+	query_set2 = Experience.objects.all()
+	context = {'Education' : query_set, 'Experience': query_set2}
 	return render(request, 'resume.html', context)
