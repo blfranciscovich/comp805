596efbc844bc55ac21cf0b880f7c71e08f286f67
localedtech <bridget@localed.co>
2018-02-20 16:22:24 -0500

    tests finished

--- week4/project1/resume/tests.py
+++ week4/project1/resume/tests.py
@@ -1,47 +1,46 @@
 from django.test import TestCase
-from .models import Education, Experience, Resume
-import unittest
+from .models import Resume, Education, Experience
 
 # Create your tests here.
-class TestResume (unittest.TestCase):
+class TestResume (TestCase):
+    test_resume = Resume.objects.create(first_name = 'Bridget',
+        last_name = 'Franciscovich')
+    test_education = Education.objects.create(parent_resume = test_resume,
+        institution_name = 'UNH', location = 'New Hampshire',
+        degree = 'Master of Science', major = 'IT', gpa = 4.0)
+    test_experience = Experience.objects.create(parent_resume = test_resume,
+        title = 'Marketing Manager', location = 'New Hampshire',
+        start_date = '2011-01-01',end_date = '2022-02-02', description = 'Marketing is fun!')
 
     def setUp(self):
-        self.resume.save()
-        self.education.save()
-        self.experience.save()
+        self.test_resume.save()
+        self.test_education.save()
+        self.test_experience.save()
 
     def test_get_full_name(self):
         """
         Tests models.get_full_name(self):
         """
         expected = 'Bridget Franciscovich'
-        self.assertEqual(self.resume.get_full_name(), expected)
+        resume = Resume.objects.first()
+        self.assertEqual(resume.get_full_name(), expected)
 
-    def test_get_last_name_first_name(self):
+    def test_get_last_first_name(self):
         """
         Tests models.get_last_first_name(self):
         """
-        expected = 'Franciscovich, Bridget'
-        self.assertEqual(self.resume.test_get_last_name_first_name(), expected)
+        expected = 'Franciscovich Bridget'
+        resume = Resume.objects.first()
+        self.assertEqual(resume.get_last_first_name(), expected)
 
     def test_get_education(self):
         """
         Tests models.get_education(self):
         """
-        expected = list(education.education.all())
-        self.assertEqual(self.resume.get_education().first(), expected)
+        self.assertEqual(self.test_resume.get_education().first(), self.test_education)
 
     def test_get_experience(self):
         """
         Tests models.get_experience(self):
         """
-        expected = list(experience.experience_set.all())
-        self.assertEqual(self.resume.get_experience().first(), expected)
-
-if __name__ == '__main__':
-    try:
-        import models
-        print("models.py module found.Testing...")
-        unittest.main( )
-    except ImportError:
-        print("Missing models.py module")
+        self.assertEqual(self.test_resume.get_experience().first(), self.test_experience)
