d4baf77d736f0034e1363b2144bb67ae22c64385
localedtech <bridget@localed.co>
2018-02-19 19:38:10 -0500

    updated resume.html

--- week4/project1/resume/models.py
+++ week4/project1/resume/models.py
@@ -6,8 +6,29 @@ class Resume(models.Model):
 	first_name = models.CharField(max_length=64)
 	last_name = models.CharField(max_length=64)
 
-	def last_name_first(self):
+	def get_full_name(self):
+		"""
+		Returns full name in the following order: First name, Last Name.
+		"""
+		return "{} {}".format(self.first_name, self.last_name)
+
+	def get_last_first_name(self):
+		"""
+		Returns the full name in the following order: Last Name, First Name.
+		"""
 		return "{} {}".format(self.last_name, self.first_name)
+
+	def get_education(self):
+		"""
+		Returns all education related to the resume based on a foreign key to education model.
+		"""
+		return self.education_set.all()
+
+	def get_experience(self):
+		"""
+		Returns all experience related to the resume based on a foreign key to experience model.
+		"""
+		return self.experience_set.all()
 
 class Experience(models.Model):
 	parent_resume = models.ForeignKey('Resume', on_delete=models.CASCADE, default=1)
@@ -18,6 +39,7 @@ class Experience(models.Model):
 	description = models.TextField(max_length=150, null=False, blank=False)
 
 class Education(models.Model):
+	parent_resume = models.ForeignKey('Resume', on_delete=models.CASCADE, default=1)
 	institution_name = models.CharField(max_length=64, null=False, blank=False)
 	location = models.CharField(max_length=64, null=False, blank=False)
 	degree = models.CharField(max_length=64, null=False, blank=False)
--- week4/project1/resume/views.py
+++ week4/project1/resume/views.py
@@ -1,15 +1,23 @@
 from django.shortcuts import render
-from .models import Education, Experience
+from .models import Education, Experience, Resume
 
 # Create your views here.
 
-def resume(request):
+#def resume(request):
+#	"""
+#	This function returns education and experience objects in the URL resume/.
+#	request: URL request resume/ from user
+#	return: the URL resume/ with all education and experience objects
+#	"""
+#	query_set = Education.objects.all()
+#	query_set2 = Experience.objects.all()
+#	context = {'Education' : query_set, 'Experience': query_set2}
+#	return render(request, 'resume/resume.html', context)
+def resume (request):
 	"""
 	This function returns education and experience objects in the URL resume/.
 	request: URL request resume/ from user
 	return: the URL resume/ with all education and experience objects
 	"""
-	query_set = Education.objects.all()
-	query_set2 = Experience.objects.all()
-	context = {'Education' : query_set, 'Experience': query_set2}
-	return render(request, 'resume.html', context)
+	resume = Resume.objects.first()
+	return render(request, 'resume/resume.html', context={'resume': resume})
